c35a336e9971b0c6cfbd65a9c6d9ae1e79215eb3
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-05-06 17:56:35 -0400

    scope specs better, add a spec for the classifier

--- spec/extensions_spec.rb
+++ spec/extensions_spec.rb
@@ -12,14 +12,26 @@ end
 describe Schemad::Extensions do
   Given(:ext) { Demo::Foo::Bar.new }
 
-  When(:class_base) { Demo::Foo::Bar.base_class_name }
-  Then { class_base.should == "Bar" }
+  context "base class name getter" do
+    When(:class_base) { Demo::Foo::Bar.base_class_name }
+    Then { class_base.should == "Bar" }
+  end
 
-  When(:base) { ext.base_class_name }
-  Then { base.should == "Bar" }
+  context "instance method for base class" do
+    When(:base) { ext.base_class_name }
+    Then { base.should == "Bar" }
+  end
 
-  When(:const) { ext.send :constantize, "Demo::Foo::Bar" }
-  Then { const.should == Demo::Foo::Bar }
+  context "constantizer" do
+    When(:const) { ext.send :constantize, "Demo::Foo::Bar" }
+    Then { const.should == Demo::Foo::Bar }
+  end
+
+  context "classifier" do
+    When(:classy) { ext.send :classify, "demo_foo_bar" }
+    Then { classy.should == "DemoFooBar" }
+  end
+
   context "indifferent hash wrapper" do
     When(:converted) { ext.send :indifferent_hash, {one: 1, "two" => 2, "Three and Four" => 34} }
     Then { converted["one"].should == 1 }
