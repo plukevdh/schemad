d8b90485d632c2863b5e12fdd1a366ee4ecca1d6
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-05-08 14:33:18 -0400

    default functionality set

--- lib/schemad.rb
+++ lib/schemad.rb
@@ -1,5 +1,7 @@
 require "schemad/version"
 
-module Schemad
-  # Your code goes here...
-end
+require 'schemad/type_handler'
+require 'schemad/normalizer'
+require 'schemad/entity'
+
+module Schemad ; end
